28f8d0f68522729e4df0d90f02edf18f16a9f6a2
benoitl <benoit.lavigne@windriver.com>
2011-03-25 10:25:47 -0700

    Fix problem with tag stats and added user stats

---
+++ askbot/management/commands/get_user_stats.py
@@ -0,0 +1,44 @@
+import sys
+import optparse
+import datetime
+from django.core.management.base import BaseCommand, CommandError
+from askbot import models
+
+
+def format_table_row(*cols, **kwargs):
+        bits = list()
+        for col in cols:
+            bits.append(col)
+        line = kwargs['format_string'] % tuple(bits)
+
+        return line
+
+
+class Command(BaseCommand):
+    help = """Dump user reputation statistics."""
+
+    option_list = BaseCommand.option_list 
+    def handle(self, *args, **options):
+        print 'User Stats for %s\n' % datetime.date.today()
+        self.print_user_sub_counts()
+        print ''
+
+    def print_user_sub_counts(self):
+        """prints list of users and their stats
+        """
+        users = models.User.objects.all().order_by('username')
+        item_count = 0
+        print '%-20s %6s %6s %6s %6s %6s %6s %6s' % (
+	    'User (id)', '# Q', '# A', 'Votes', 'Karma','Gold','Silver','Bronze')
+        print '%-20s %6s %6s %6s %6s %6s %6s %6s' % (
+	    '===================', '===', '===', '=====', '=====', '====', '======', '======')
+        for user in users:
+            user_string = '%s (%d)' % (user.username, user.id)
+            line = format_table_row(
+                                user_string, user.questions.count(), user.answers.count(),
+				user.votes.count(), 
+                                user.reputation, user.gold, user.silver, user.bronze,
+                                format_string = '%-20s %6s %6s %6s %6s %6s %6s %6s'
+                            )
+            print line
+
