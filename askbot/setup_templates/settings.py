1d6cfeffd95fa5d815b9894d10f421afa3c77314
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-04-09 17:59:07 -0400

    added postman app to the list of installed apps in the sample settings.py

--- askbot/setup_templates/settings.py
+++ askbot/setup_templates/settings.py
@@ -163,6 +163,7 @@ INSTALLED_APPS = (
     'django_countries',
     'djcelery',
     'djkombu',
+    'askbot.deps.postman',
     #'avatar',#experimental use git clone git://github.com/ericflo/django-avatar.git$
     #requires setting of MEDIA_ROOT and MEDIA_URL
 )
