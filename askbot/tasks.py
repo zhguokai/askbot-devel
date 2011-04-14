8f84bf2fbe3be84e9b39b15d127def3458dafa3b
benoitl <benoit.lavigne@windriver.com>
2011-04-14 06:49:13 -0700

    Print email stats and measurements

--- askbot/tasks.py
+++ askbot/tasks.py
@@ -1,8 +1,27 @@
+import logging
+import time
 from django.contrib.contenttypes.models import ContentType
 from celery.decorators import task
 from askbot.models import Activity
 from askbot.models import User
 from askbot.models import send_instant_notifications_about_activity_in_post
+def get_subs_email(user_list):
+     users = " (%d):" % len(user_list)
+     cnt = len(users)
+     pad = " " * cnt
+     emails = []
+     for user in user_list:
+        emails.append(user.email.split('@')[0].lower())
+     emails.sort()
+     for ustr in emails:
+         if(cnt + len(ustr) > 75):
+            users +="\n" + pad 
+            cnt = len(pad)
+         users += " %s" % ustr
+         cnt += len(ustr) + 1
+
+     return users 
+
 
 @task(ignore_results = True)
 def record_post_update_task(
@@ -17,6 +36,7 @@ def record_post_update_task(
     updated_by = User.objects.get(id = updated_by_id)
     post_content_type = ContentType.objects.get(id = post_content_type_id)
     post = post_content_type.get_object_for_this_type(id = post_id)
+    start_time = time.time()
 
     #todo: take into account created == True case
     (activity_type, update_object) = post.get_updated_activity_data(created)
@@ -53,13 +73,14 @@ def record_post_update_task(
     #todo: weird thing is that only comments need the recipients
     #todo: debug these calls and then uncomment in the repo
     #argument to this call
+    pre_notif_time = time.time()
     notification_subscribers = post.get_instant_notification_subscribers(
                                     potential_subscribers = recipients,
                                     mentioned_users = newly_mentioned_users,
                                     exclude_list = [updated_by, ]
                                 )
     #todo: fix this temporary spam protection plug
-    if created:
+    if False:
         if not (updated_by.is_administrator() or updated_by.is_moderator()):
             if updated_by.reputation < 15:
                 notification_subscribers = \
@@ -68,8 +89,15 @@ def record_post_update_task(
     #Updater always gets an email
     notification_subscribers.append(updated_by)
 
+    pre_email_time = time.time()
     send_instant_notifications_about_activity_in_post(
                             update_activity = update_activity,
                             post = post,
                             recipients = notification_subscribers,
                         )
+    debug_str = "\nEmailed%s\n" % get_subs_email(notification_subscribers)
+    debug_str += "  Pre-notif Time: %8.3f\n" % float(pre_notif_time - start_time)
+    debug_str += "  Sub Search Time: %8.3f\n" % float(pre_email_time - pre_notif_time)
+    debug_str += "  Email Time: %8.3f\n" % float(time.time() - pre_email_time)
+    debug_str += "Total Elapsed Time: %8.3f" % float(time.time() - start_time)
+    logging.critical(debug_str)
