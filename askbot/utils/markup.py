733bfbf76052b79d84e8119f07b68ad91543ad10
benoitl <benoit.lavigne@windriver.com>
2011-06-02 06:29:07 -0700

    Added TSR autolink

--- askbot/utils/markup.py
+++ askbot/utils/markup.py
@@ -8,10 +8,12 @@ from markdown2 import Markdown
 URL_RE = re.compile("((?<!(href|.src|data)=['\"])((http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*))")
 
 BUG_RE = re.compile(r'(?<![[/])WIND(\d\d\d\d\d\d\d\d)\b')
+TSR_RE = re.compile(r'(?<![[/])TSR-?(\d\d\d\d\d+)\b')
 
 LINK_PATTERNS = [
     (URL_RE, r'\1'),
     (BUG_RE, django_settings.BUG_DB_LINK + r'\1'),
+    (TSR_RE, django_settings.TSR_DB_LINK + r'\1'),
 ]
 
 def get_parser():
