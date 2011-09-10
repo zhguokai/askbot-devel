632455cb0fb92bcdf3c067678bdc3f8e7f89776b
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-09-10 10:36:06 -0300

    fixed bugs in the import statement of django_authopenid.conf

--- askbot/deps/django_authopenid/conf.py
+++ askbot/deps/django_authopenid/conf.py
@@ -1,16 +1,13 @@
 """unifies django settings with an optional additional settings module
 and default settings of this application"""
 from django.conf import settings as django_settings
-from askbot.deps.django_authopenid import default_settings
-from import_utils import import_module_from
 from multi_registry import MultiRegistry
 
-settings = MultiRegistry()
-settings.append(django_settings)
+settings = MultiRegistry(
+    'django.conf.settings',
+    'askbot.deps.django_authopenid.default_settings'
+)
 
 extra_settings_path = getattr(django_settings, 'EXTRA_SETTINGS_MODULE', None)
 if extra_settings_path:
-    module = import_module_from(extra_settings_path)
-    settings.append(module)
-
-settings.append(default_settings)
+    settings.insert(1, extra_settings_path)
