2b73ec1e8c7a7fe88acae380c3947782d3759206
Artyom Shein <temiy@aisys.ru>
2012-01-04 22:44:11 +0400

    Lost changes from previous commit. Now chart_data respects Chart.auth_required. Add data caching for Chart.

--- chart/context.py
+++ chart/context.py
@@ -1,26 +1,33 @@
 import sys
+from django.core.urlresolvers import reverse
 from chart.models import Chart
+from chart.views import chart_data
+from django.http import HttpResponseForbidden
 
 def charts(request):
-	def insert_chart(pk, el_id):
+	def insert_chart(pk, el_id, ajax=False):
 		try:
 			chart = Chart.objects.get(pk=pk)
 		except:
 			return '<!-- Error: requested chart not found! -->'
-		path = chart.data_provider.rsplit('.', 1)
-		data = None
-		#try:
-		__import__(path[0])
-		view = getattr(sys.modules[path[0]], path[1])
-		data = view(request).content
-		#except:
-		#	return '<!-- Error: requested chart\'s data provider not found! -->'
-		
-		return '<script type="text/javascript" language="JavaScript">//<![CDATA[\n$.jqplot("%(id)s", %(data)s, %(options)s);\n//]]></script>' % {
-			'id': str(el_id),
-			'data': data,
-			'options': any(chart.options) and chart.options or 'null',
-		}
+		def script(script_text):
+			return '<script type="text/javascript" language="JavaScript">//<![CDATA[\n%s\n//]]></script>' \
+				% script_text
+		if ajax:
+			return script('ajaxChart("%(id)s", "%(path)s", %(options)s);'
+				% {
+					'id': unicode(el_id),
+					'path': reverse('chart_data', kwargs={'chart_pk': pk}),
+					'options': any(chart.options) and chart.options or 'null',})
+		else:
+			data_response = chart_data(request, pk)
+			if isinstance(data_response, HttpResponseForbidden):
+				return '<!-- Error: authorisation required -->'
+			return script('$.jqplot("%(id)s", %(data)s, %(options)s);'
+				% {
+					'id': unicode(el_id),
+					'data': data_response.content,
+					'options': any(chart.options) and chart.options or 'null',})
 	return {
 		'insert_chart': insert_chart,
 	}
--- chart/models.py
+++ chart/models.py
@@ -1,16 +1,58 @@
 from django.db import models
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from datetime import datetime, timedelta
 
 class Chart(models.Model):
-	"""Chart"""
-	options = models.TextField(null=True, blank=True)
-	data_provider = models.TextField()
-	auth_required = models.BooleanField(default=False)
-	
-	class Meta:
-		app_label = 'chart'
+    """Chart"""
+    options = models.TextField(blank=True)
+    data_provider = models.CharField(max_length=65535)
+    auth_required = models.BooleanField(
+        default=False, verbose_name=_('authorisation required'))
+    data_cache_period = models.PositiveIntegerField(
+        default=0, verbose_name=_('data cache period in seconds'),
+        help_text=_('0 &mdash; no cache'))
+    data_cache = models.TextField(blank=True, null=True)
+    data_cache_datetime = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        app_label = 'chart'
+    
+    def is_data_caching_enabled(self):
+        return 0 != self.data_cache_period
+    
+    def is_data_cache_empty(self):
+        return '' == self.data_cache
+    
+    def set_data_cache(self, data):
+        self.data_cache = data
+        self.data_cache_datetime = datetime.now()
+    
+    def is_data_cache_valid(self):
+        if not self.is_data_caching_enabled() \
+                or not self.data_cache_datetime \
+                or '' == self.data_cache:
+            return False
+        now = datetime.now()
+        cache_ends = self.data_cache_datetime + timedelta(seconds=self.data_cache_period)
+        return cache_ends >= now
+
+def invalidate_charts_data_cache(modeladmin, request, queryset):
+    queryset.update(data_cache='')
+invalidate_charts_data_cache.short_description = _("Invalidate data cache")
 
 class ChartAdmin(admin.ModelAdmin):
     """Chart admin class"""
+    fieldsets = (
+        (None, {
+            'fields': ('data_provider', 'auth_required', 'options'),
+        }),
+        (_('Cache'), {
+            'classes': ('collapse',),
+            'fields':
+                ('data_cache_period', 'data_cache_datetime', 'data_cache'),
+        })
+    )
+    actions = [invalidate_charts_data_cache]
 
 admin.site.register(Chart, ChartAdmin)
