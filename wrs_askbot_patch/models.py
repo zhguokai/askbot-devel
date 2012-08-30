421f0be45fc478ea53f71639d5232132e35d345d
benoitl <benoit.lavigne@windriver.com>
2012-08-30 07:42:24 -0700

    Removed old company specific migrations

--- wrs_askbot_patch/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from askbot.models import MarkedTag
-
-#field to store "subscribed wildcard tags"
-User.add_to_class('subscribed_tags', models.TextField(blank = True))
