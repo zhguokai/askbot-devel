e7f819c2bca3176f11582187e0f992c978fbb510
b1uebyte <andryuhell@gmail.com>
2013-04-06 02:18:53 +0400

    locale update

--- askbot/media/js/live_search.js
+++ askbot/media/js/live_search.js
@@ -267,7 +267,7 @@ TagWarningBox.prototype.showWarning = function(){
  */
 var InputToolTip = function() {
     WrappedElement.call(this);
-    this._promptText = gettext('search or ask your question');
+    this._promptText = gettext('поиск по вопросам');
 };
 inherits(InputToolTip, WrappedElement);
 
