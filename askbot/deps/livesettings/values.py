637052ee0353087af95eb0b9dc0754c875db30f4
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2013-04-21 19:03:13 -0400

    styling + a unicode livesettings bugfix

--- askbot/deps/livesettings/values.py
+++ askbot/deps/livesettings/values.py
@@ -9,7 +9,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.cache import cache
 from django.utils import simplejson
 from django.utils.datastructures import SortedDict
-from django.utils.encoding import smart_str
+from django.utils.encoding import force_unicode
 from django.utils.translation import ugettext as _
 from django.utils.translation import ugettext_lazy
 from django.core.files import storage
@@ -246,11 +246,11 @@ class Value(object):
                 work = []
                 for x in self.choices:
                     if x[0] in self.default:
-                        work.append(smart_str(x[1]))
-                note = _('Default value: ') + ", ".join(work)
+                        work.append(force_unicode(x[1]))
+                note = _('Default value: ') + unicode(u", ".join(work))
 
             else:
-                note = _("Default value: %s") % unicode(self.default)
+                note = _("Default value: %s") % force_unicode(self.default)
 
         return note
 
