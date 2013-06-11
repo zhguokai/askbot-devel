aec40e5fb1a158001156983d80a2526521e75f91
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2013-06-11 11:29:12 -0600

    created signal handler for askbot's signals

---
+++ askbot/search/haystack/signals.py
@@ -0,0 +1,21 @@
+from django.db.models import signals as django_signals
+from askbot.models import signals as askbot_signals
+from haystack.signals import BaseSignalProcessor
+
+class AskbotRealtimeSignalProcessor(BaseSignalProcessor):
+    '''
+    Based on haystack RealTimeSignalProcessor with some
+    modifications to work with askbot soft-delete models
+    '''
+
+    def setup(self):
+        django_signals.post_save.connect(self.handle_save)
+        django_signals.post_delete.connect(self.handle_delete)
+        #askbot signals
+        askbot_signals.delete_question_or_answer.connect(self.handle_delete)
+
+    def teardown(self):
+        django_signals.post_save.disconnect(self.handle_save)
+        django_signals.post_delete.disconnect(self.handle_delete)
+        #askbot signals
+        askbot_signals.delete_question_or_answer.disconnect(self.handle_delete)
