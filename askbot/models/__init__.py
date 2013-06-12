ac4f38da19977571ddf3b6e2403eb1813c93c149
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2013-06-12 15:20:26 -0600

    fixed bug

--- askbot/models/__init__.py
+++ askbot/models/__init__.py
@@ -3760,8 +3760,8 @@ signals.new_question_posted.connect(tweet_new_post)
 signals.post_revision_published.connect(notify_author_of_published_revision)
 signals.site_visited.connect(record_user_visit)
 
-if getattr('HAYSTACK_SIGNAL_PROCESSOR',
-           django_settings, ' ').endswith('AskbotRealtimeSignalProcessor'):
+if getattr(django_settings, 'HAYSTACK_SIGNAL_PROCESSOR',
+           ' ').endswith('AskbotRealtimeSignalProcessor'):
     from haystack import signal_processor
     signal_processor.teardown()
     signal_processor.setup()
