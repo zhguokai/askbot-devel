e73be3ef8443378c883c288a10e3381f1b871387
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2013-08-04 16:02:20 -0400

    hopefully fixes user search in solr

--- askbot/search/haystack/__init__.py
+++ askbot/search/haystack/__init__.py
@@ -41,14 +41,19 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
         ALLOWED_TYPES = ('question', 'answer', 'comment')
+        model_cls = self.get_model()
         if getattr(settings, 'ASKBOT_MULTILINGUAL', True):
             lang_code = get_language()[:2]
-            return self.get_model().objects.filter(language_code__startswith=lang_code,
-                                                   deleted=False,
-                                                   post_type__in=ALLOWED_TYPES)
+            return model_cls.objects.filter(
+                                    language_code__startswith=lang_code,
+                                    deleted=False,
+                                    post_type__in=ALLOWED_TYPES
+                                )
         else:
-            return self.get_model().objects.filter(deleted=False,
-                                                   post_type__in=ALLOWED_TYPES)
+            return model_cls.objects.filter(
+                                    deleted=False,
+                                    post_type__in=ALLOWED_TYPES
+                                )
 
 class UserIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
