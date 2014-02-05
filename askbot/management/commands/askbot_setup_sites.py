6b05b209f82b8ca49f8a9ba4c1e631fa4dd0ce7f
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2014-02-05 10:33:27 -0300

    finished the askbot_setup_sites management command

--- askbot/management/commands/askbot_setup_sites.py
+++ askbot/management/commands/askbot_setup_sites.py
@@ -6,29 +6,29 @@ from django.conf import settings as django_settings
 from django.core.management.base import NoArgsCommand
 from django.contrib.sites.models import Site
 
-SITE1_DOMAIN = getattr(django_settings, 'SITE1_DOMAIN', 'windriver.askbot.com')
-SITE1_NAME = getattr(django_settings, 'SITE1_NAME', 'windriver')
-SITE2_DOMAIN = getattr(django_settings, 'SITE2_DOMAIN', 'intel.askbot.com')
-SITE2_NAME = getattr(django_settings, 'SITE2_NAME', 'intel')
-
 """examples:
 #key - space id, value - space name
+#here we create two question spaces
 ASKBOT_SPACES = {
     1: 'support',
     2: 'development'
 }
 #key - site id, value - (site name, site domain)
+#here we have two sites
 ASKBOT_SITES = {
     1: ('public', 'public.example.com'),
     2: ('private', 'private.example.com')
 }
 #key - feed id, value - (feed name, site id, (space id list))
+#two feeds, both named 'questions', associated with sites 1 and 2
+#and fist one has space 1, while second - spaces 1 and 2
 ASKBOT_FEEDS = {
     1: ('questions', 1, (1,)),
     2: ('questions', 2, (1, 2))
 }
 """
 
+SITES = getattr(django_settings, 'ASKBOT_SITES')
 SPACES = getattr(django_settings, 'ASKBOT_SPACES')
 FEEDS = getattr(django_settings, 'ASKBOT_FEEDS')
 
@@ -48,7 +48,7 @@ class Command(NoArgsCommand):
             space.save()
             
         #create sites
-        for (site_id, site_data) in SITES.items()
+        for (site_id, site_data) in SITES.items():
             site = get_object_by_id(Site, site_id)
             site.name = site_data[0]
             site.domain = site_data[1]
@@ -56,19 +56,29 @@ class Command(NoArgsCommand):
 
         #create feeds
         for (feed_id, feed_data) in FEEDS.items():
-            feed = get_object_by_id(Feed, 1)
-            feed.name = 'questions'
-            feed.default_space = space1
-            feed.site = site1
+            feed = get_object_by_id(Feed, feed_id)
+            feed.name = feed_data[0]
+            site_id = feed_data[1]
+            space_ids = feed_data[2]
+            feed.default_space = get_object_by_id(Space, space_ids[0])
+            feed.site = get_object_by_id(Site, site_id)
             feed.save()
 
-            for space_id in feed_data[1:]:
+            for space_id in space_ids:
                 space = get_object_by_id(Space, space_id)
                 feed.add_space(space)
 
+        #get site with lowest id:
+        main_site = Site.objects.all().order_by('id')[0]
+        #naive way to get the main feed for the site
+        main_feed = Feed.objects.filter(
+                                site=main_site
+                            ).order_by('id')[0]
+
         threads = Thread.objects.all()
         count = threads.count()
-        message = 'Adding all threads to the %s space' % SITE1_NAME
+        message = 'Adding all threads to the %s space' % main_site.name
 
+        main_space = main_feed.default_space
         for thread in ProgressBar(threads.iterator(), count, message):
-            space1.questions.add(thread)
+            main_space.questions.add(thread)
