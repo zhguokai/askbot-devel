2bd2e572568312c0df52197836f772a5faee0f5c
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2014-04-03 09:25:38 -0300

    added site id to the logging message

--- askbot/utils/debug.py
+++ askbot/utils/debug.py
@@ -1,7 +1,10 @@
 """debugging utilities"""
 import sys
+from django.config import settings
 
 def debug(message):
     """print debugging message to stderr"""
+    site_id = django_settings.SITE_ID
+    message = 'site_id=' + site_id + ' ' + message
     message = unicode(message).encode('utf-8')
     sys.stderr.write(message + '\n')
