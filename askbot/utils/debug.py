2fa38275c5fd91e31968af4c93180086c668e8dc
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2014-04-04 00:43:32 -0300

    a bug fix

--- askbot/utils/debug.py
+++ askbot/utils/debug.py
@@ -5,6 +5,6 @@ from django.conf import settings as django_settings
 def debug(message):
     """print debugging message to stderr"""
     site_id = django_settings.SITE_ID
-    message = 'site_id=' + site_id + ' ' + message
+    message = 'site_id=' + str(site_id) + ' ' + message
     message = unicode(message).encode('utf-8')
     sys.stderr.write(message + '\n')
