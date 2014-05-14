0bfe107f9875d68976dfbd0fa56416672bf9e123
jogwen <joanna.wakeford+github@gmail.com>
2014-05-14 14:44:54 +0100

    Fixed bug in debug where 'message' is not a string, plus include a timestamp in the message.

--- askbot/utils/debug.py
+++ askbot/utils/debug.py
@@ -1,10 +1,14 @@
 """debugging utilities"""
+import datetime
 import sys
 from django.conf import settings as django_settings
 
 def debug(message):
     """print debugging message to stderr"""
     site_id = django_settings.SITE_ID
-    message = 'site_id=' + str(site_id) + ' ' + message
+    message = "%s site_id=%s %s" % (
+        datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
+        site_id, 
+        message)
     message = unicode(message).encode('utf-8')
     sys.stderr.write(message + '\n')
