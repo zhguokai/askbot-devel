64491af526128079f3525382e42ea4cff95c8c40
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2014-06-16 14:08:00 -0300

    added site_id parameter to askbot_fix_tag_used_counts

--- askbot/management/commands/askbot_fix_tag_used_counts.py
+++ askbot/management/commands/askbot_fix_tag_used_counts.py
@@ -1,10 +1,11 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from django.contrib.sites.models import Site
 from askbot.conf import settings as askbot_settings
 from askbot.models import Space
 from askbot.models import Tag
 from askbot.models import Thread
 import askbot
+from optparse import make_option
 
 def get_threads_counts(tag, site):
     """returns number of threads on current site
@@ -17,9 +18,20 @@ def get_threads_counts(tag, site):
         site_count = threads.distinct().count()
     return total_count, site_count
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, *args, **kwargs):
-        site = Site.objects.get_current()
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--site-id', 
+            action='store', 
+            dest='site_id', 
+            default=None
+        ),
+    )
+    def handle(self, *args, **kwargs):
+        if 'site_id' in kwargs:
+            site = Site.objects.get(id=kwargs['site_id'])
+        else:
+            site = Site.objects.get_current()
         for tag in Tag.objects.all():
             total_count, site_count = get_threads_counts(tag, site)
             tag.used_count = total_count
