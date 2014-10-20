1148c640a04773aa204d97c257e81ae1414f346d
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2014-10-20 19:24:45 -0300

    removed askbot global css from the ask widget

--- askbot/templates/embed/askbot_widget.js
+++ askbot/templates/embed/askbot_widget.js
@@ -1,79 +1,78 @@
 var {{variable_name}} = {
-  element_id: "{{variable_name}}",
-  widgetToggle: function() {
-    element = document.getElementById({{variable_name}}.element_id);
-    element.style.visibility = (element.style.visibility == "visible") ? "hidden" : "visible";
-    if (element.style.visibility == "visible"){
-      $("#" + {{variable_name}}.element_id + " iframe").focus();
+    element_id: "{{variable_name}}",
+    widgetToggle: function() {
+        element = document.getElementById({{variable_name}}.element_id);
+        element.style.visibility = (element.style.visibility == "visible") ? "hidden" : "visible";
+        if (element.style.visibility == "visible"){
+            var focusInIFrame = function() {
+                var iframe = document.getElementById({{ variable_name }}.element_id + 'iframe');
+                iframe.focus();
+            }
+            setTimeout(focusInIFrame, 100);
+        }
+    },
+    toHtml: function() {
+        var html = {{ variable_name }}.createButton();
+        var link = document.createElement('link');
+        var protocol = 'http:'//document.location.protocol;
+
+        //widget css
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("href", protocol + '//{{host}}{% url render_ask_widget_css widget.id %}');
+
+        //creating the div
+        var motherDiv = document.createElement('div');
+        motherDiv.setAttribute("id", {{ variable_name }}.element_id);
+        motherDiv.style.visibility = "hidden";
+
+        var containerDiv = document.createElement('div');
+        motherDiv.appendChild(containerDiv);
+
+        {% if widget.outer_style %}
+        var outerStyle = document.createElement('style');
+        outerStyle.innerText = "{{widget.outer_style}}";
+        motherDiv.appendChild(outerStyle);
+        {% endif %}
+
+        var title = document.createElement('h1');
+        title.innerHTML = '{{ widget.title }}';
+
+        containerDiv.appendChild(title);
+
+        var closeButton = document.createElement('a');
+        closeButton.setAttribute('href', '#');
+        closeButton.setAttribute('onClick', '{{variable_name}}.widgetToggle();');
+        closeButton.setAttribute('class', 'AskbotClosePopup');
+        closeButton.innerHTML= 'x';
+
+        containerDiv.appendChild(closeButton);
+
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('id', {{ variable_name }}.element_id + 'iframe');
+        iframe.setAttribute('frameBorder', '0');
+        iframe.setAttribute('src', protocol + '//{{host}}{% url ask_by_widget widget.id %}');
+
+        containerDiv.appendChild(iframe);
+
+        var body = document.getElementsByTagName('body')[0];
+        if (body){
+            body.appendChild(link);
+            body.appendChild(motherDiv);
+        }
+    },
+    createButton: function() {
+        var label="{{ widget.title }}"; //TODO: add to the model
+        var buttonDiv = document.createElement('div');
+        buttonDiv.setAttribute('id', "AskbotAskButton" + '{{ widget.id }}');
+
+        var closeButton = document.createElement('input');
+        closeButton.setAttribute('onClick', '{{variable_name}}.widgetToggle();');
+        closeButton.setAttribute('type', 'button');
+        closeButton.value = label;
+
+        buttonDiv.appendChild(closeButton);
+        return buttonDiv;
     }
-  },
-  toHtml: function() {
-    var html = {{variable_name}}.createButton();
-    var link = document.createElement('link');
-    var protocol = document.location.protocol;
-
-    //widget css
-    link.setAttribute("rel", "stylesheet");
-    link.setAttribute("href", protocol + '//{{host}}{% url render_ask_widget_css widget.id %}');
-
-    //creating the div
-    var motherDiv = document.createElement('div');
-    motherDiv.setAttribute("id", {{variable_name}}.element_id);
-    motherDiv.style.visibility = "hidden";
-
-    var containerDiv = document.createElement('div');
-    motherDiv.appendChild(containerDiv);
-
-    {% if widget.outer_style %}
-    var outerStyle = document.createElement('style');
-    outerStyle.innerText = "{{widget.outer_style}}";
-    motherDiv.appendChild(outerStyle);
-    {% endif %}
-
-    var title = document.createElement('h1');
-    title.innerHTML = '{{ widget.title }}';
-
-    containerDiv.appendChild(title);
-
-    var closeButton = document.createElement('a');
-    closeButton.setAttribute('href', '#');
-    closeButton.setAttribute('id', 'AskbotModalClose');
-    closeButton.setAttribute('onClick', '{{variable_name}}.widgetToggle();');
-    closeButton.innerHTML= 'Close';
-
-    containerDiv.appendChild(closeButton);
-
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('frameBorder', '0');
-    iframe.setAttribute('src', protocol + '//{{host}}{% url ask_by_widget widget.id %}');
-
-    containerDiv.appendChild(iframe);
-
-    var body = document.getElementsByTagName('body')[0];
-    if (body){
-      body.appendChild(link);
-      //login menu css
-      var link2 = document.createElement('link');
-      link2.setAttribute('rel', 'stylesheet');
-      link2.setAttribute('href', protocol + '//{{ host }}{{ '/style/style.css'|media }}');
-      body.appendChild(link2);
-      body.appendChild(motherDiv);
-    }
-  },
-  createButton: function() {
-    var label="{{ widget.title }}"; //TODO: add to the model
-    var buttonDiv = document.createElement('div');
-    buttonDiv.setAttribute('id', "AskbotAskButton");
-
-    var closeButton = document.createElement('input');
-    closeButton.setAttribute('onClick', '{{variable_name}}.widgetToggle();');
-    closeButton.setAttribute('type', 'button');
-    closeButton.value = label;
-
-    buttonDiv.appendChild(closeButton);
-    
-    return buttonDiv;
-  }
 };
 
 var askbot = askbot || {};
@@ -82,10 +81,10 @@ askbot['widgets'] = askbot['widgets'] || {};
 if (askbot['widgets']['{{ variable_name }}'] === undefined) {
     var previous_function_{{ variable_name }} = window.onload;
     var onload_functions = function(){
-      if (previous_function_{{ variable_name }}){
+    if (previous_function_{{ variable_name }}){
         previous_function_{{ variable_name }}();
-      }
-      {{variable_name}}.toHtml();
+    }
+    {{variable_name}}.toHtml();
     }
     window.onload = onload_functions;
     askbot['widgets']['{{ variable_name }}'] = {{ variable_name }};
