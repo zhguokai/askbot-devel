de4e641740253ed9931d3e6a9741b9dc21ebd019
n3ssi3 <vanessa.haenni@gmail.com>
2014-12-17 12:53:09 +0100

    move askbot custom js to separate folder

--- askbot/media/js/compress.bat
+++ askbot/media/js/compress.bat
@@ -1,5 +1,5 @@
 #java -jar yuicompressor-2.4.2.jar --type js  --charset utf-8 plugins\wmd\wmd.js -o plugins\wmd\wmd-min.js
 #java -jar yuicompressor-2.4.2.jar --type js  --charset utf-8 plugins\wmd\showdown.js -o plugins\wmd\showdown-min.js
-#java -jar yuicompressor-2.4.2.jar --type js  --charset utf-8 post.js -o post.pack.js
+#java -jar yuicompressor-2.4.2.jar --type js  --charset utf-8 askbot/post.js -o askbot/post.pack.js
 java -jar yuicompressor-2.4.2.jar --type js  --charset utf-8 libs\se_hilite_src.js -o libs\se_hilite.js
 pause
