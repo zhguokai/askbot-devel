d0a26740ad97c1fc46376bdb62f7393dc3cbc605
Vadim Sikora <vadim.sikora@gmail.com>
2014-12-19 10:49:53 +0100

    fix mixed tabs and spaces

--- askbot/media/js/jquery.openid.js
+++ askbot/media/js/jquery.openid.js
@@ -72,14 +72,14 @@ var providers = $.extend({}, providers_large, providers_small);
 
 var openid = {
 
-	cookie_expires: 6*30,	// 6 months.
-	cookie_name: 'openid_provider',
-	cookie_path: '/',
+    cookie_expires: 6*30,   // 6 months.
+    cookie_name: 'openid_provider',
+    cookie_path: '/',
 
-	img_path: '/media/images/openid/',
+    img_path: '/media/images/openid/',
 
-	input_id: null,
-	provider_url: null,
+    input_id: null,
+    provider_url: null,
 
     init: function(input_id) {
 
@@ -91,35 +91,35 @@ var openid = {
 
         // add box for each provider
         for (var id in providers_large) {
-           	openid_btns.append(this.getBoxHTML(providers_large[id], 'large', '.gif'));
+            openid_btns.append(this.getBoxHTML(providers_large[id], 'large', '.gif'));
         }
         if (providers_small) {
-        	openid_btns.append('<br/>');
-	        for (id in providers_small) {
-	           	openid_btns.append(this.getBoxHTML(providers_small[id], 'small', '.ico'));
-	        }
+            openid_btns.append('<br/>');
+            for (id in providers_small) {
+                openid_btns.append(this.getBoxHTML(providers_small[id], 'small', '.ico'));
+            }
         }
 
         var box_id = this.readCookie();
         if (box_id) {
-        	this.signin(box_id, true);
+            this.signin(box_id, true);
         }
     },
     getBoxHTML: function(provider, box_size, image_ext) {
 
         var box_id = provider.name.toLowerCase();
         return '<a title="'+provider.name +'" href="javascript: openid.signin(\''+ box_id +'\');"' +
-        		' style="background: #FFF url(' + this.img_path + box_id + image_ext+') no-repeat center center" ' + 'class="' + box_id + ' openid_' + box_size + '_btn"></a>';
+                ' style="background: #FFF url(' + this.img_path + box_id + image_ext+') no-repeat center center" ' + 'class="' + box_id + ' openid_' + box_size + '_btn"></a>';
 
     },
     /* Provider image click */
     signin: function(box_id, onload) {
-    	var provider = providers[box_id];
-  		if (! provider) {
-  			return;
-  		}
-		this.highlight(box_id);
-		this.setCookie(box_id);
+        var provider = providers[box_id];
+        if (! provider) {
+            return;
+        }
+        this.highlight(box_id);
+        this.setCookie(box_id);
 
         $('#'+this.input_id).val(provider.url);
         var input = $('#'+this.input_id);
@@ -142,30 +142,30 @@ var openid = {
     },
 
     highlight: function (box_id) {
-    	// remove previous highlight.
-    	var highlight = $('#openid_highlight');
-    	if (highlight) {
-    		highlight.replaceWith($('#openid_highlight a')[0]);
-    	}
-    	// add new highlight.
-    	$('.'+box_id).wrap('<div id="openid_highlight"></div>');
+        // remove previous highlight.
+        var highlight = $('#openid_highlight');
+        if (highlight) {
+            highlight.replaceWith($('#openid_highlight a')[0]);
+        }
+        // add new highlight.
+        $('.'+box_id).wrap('<div id="openid_highlight"></div>');
     },
 
     setCookie: function (value) {
-		var date = new Date();
-		date.setTime(date.getTime()+(this.cookie_expires*24*60*60*1000));
-		var expires = '; expires='+date.toGMTString();
-		document.cookie = this.cookie_name+'='+value+expires+'; path=' + this.cookie_path;
+        var date = new Date();
+        date.setTime(date.getTime()+(this.cookie_expires*24*60*60*1000));
+        var expires = '; expires='+date.toGMTString();
+        document.cookie = this.cookie_name+'='+value+expires+'; path=' + this.cookie_path;
     },
 
     readCookie: function () {
-		var nameEQ = this.cookie_name + '=';
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)===' ') { c = c.substring(1,c.length); }
-			if (c.indexOf(nameEQ) === 0) { return c.substring(nameEQ.length,c.length); }
-		}
-		return null;
+        var nameEQ = this.cookie_name + '=';
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)===' ') { c = c.substring(1,c.length); }
+            if (c.indexOf(nameEQ) === 0) { return c.substring(nameEQ.length,c.length); }
+        }
+        return null;
     }
 };
