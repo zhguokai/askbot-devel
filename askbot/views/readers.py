59ba983760f19445fdc6f85d2d3ec406521e38d6
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2015-04-15 15:01:05 -0300

    reverted pulling related tags with poorely performing method commit 0e7ba659a627303e35e4e1d4309fd2dc33ba8ae0

--- askbot/views/readers.py
+++ askbot/views/readers.py
@@ -108,7 +108,7 @@ def questions(request, **kwargs):
     models.Thread.objects.precache_view_data_hack(threads=page.object_list)
 
     related_tags = Tag.objects.get_related_to_search(
-                        threads=qs,
+                        threads=page.object_list,
                         ignored_tag_names=meta_data.get('ignored_tag_names',[])
                     )
     tag_list_type = askbot_settings.TAG_LIST_FORMAT
