001956f543219ea9ee22020fbf7003fc782a4d25
Jonathan Stoppani <jonathan@stoppani.name>
2015-05-13 11:27:48 +0200

    Support rebuilding the cache for different languages

--- askbot/management/commands/build_thread_summary_cache.py
+++ askbot/management/commands/build_thread_summary_cache.py
@@ -1,10 +1,31 @@
-from django.core.management.base import NoArgsCommand
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from django.utils import translation
+
 from askbot.models import Thread
 from askbot.utils.console import ProgressBar
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
-        message = "Rebuilding thread summary cache"
-        count = Thread.objects.count()
-        for thread in ProgressBar(Thread.objects.iterator(), count, message):
-            thread.update_summary_html()
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '-l',
+            '--language',
+            action='append',
+            help='Specify the languages for which the cache has to be rebuilt.'
+        ),
+    )
+
+    def handle(self, **options):
+        languages = options['language']
+        if not languages:
+            languages = [translation.get_language()]
+
+        for l in languages:
+            translation.activate(l)
+            message = 'Rebuilding {} thread summary cache'.format(l.upper())
+            count = Thread.objects.count()
+            for thread in ProgressBar(Thread.objects.iterator(),
+                                      count, message):
+                thread.update_summary_html()
