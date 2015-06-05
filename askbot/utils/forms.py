35b287193d6f16be1579cc3162336bab7e58762d
jogwen <joanna.wakeford+github@gmail.com>
2015-06-05 13:48:30 +0100

    Fixed this so that it still works for emails like 'username+somealias@gmail.com'. The regex code seems like unnecessary over-complication anyway.

--- askbot/utils/forms.py
+++ askbot/utils/forms.py
@@ -185,17 +185,14 @@ def email_is_allowed(
     domain"""
     if allowed_emails:
         email_list = split_list(allowed_emails)
-        allowed_emails = ' ' + ' '.join(email_list) + ' '
-        email_match_re = re.compile(r'\s%s\s' % email, re.I)
-        if email_match_re.search(allowed_emails):
+        if email in email_list:
             return True
 
     if allowed_email_domains:
         email_domain = email.split('@')[1]
         domain_list = split_list(allowed_email_domains)
-        domain_match_re = re.compile(r'\s%s\s' % email_domain, re.I)
-        allowed_email_domains = ' ' + ' '.join(domain_list) + ' '
-        return bool(domain_match_re.search(allowed_email_domains))
+        if email_domain in domain_list:
+            return True
 
     return False
 
