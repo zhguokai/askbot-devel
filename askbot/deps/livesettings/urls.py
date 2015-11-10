ee04c33b3db613b1b9813c1a5c4c9f8eb26b4c28
Martin Burchell <martinb@aptivate.org>
2015-11-10 16:51:16 +0000

    Changed export URL to use YAML export

--- askbot/deps/livesettings/urls.py
+++ askbot/deps/livesettings/urls.py
@@ -5,6 +5,6 @@ except ImportError:
 
 urlpatterns = patterns('askbot.deps.livesettings.views',
     url(r'^$', 'site_settings', {}, name='site_settings'),
-    url(r'^export/$', 'export_as_python', {}, name='settings_export'),
+    url(r'^export/$', 'export_as_yaml', {}, name='settings_export'),
     url(r'^(?P<group>[^/]+)/$', 'group_settings', name='group_settings'),
 )
