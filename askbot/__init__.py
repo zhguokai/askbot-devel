5c70192aa8940ca1e10d836db4c07947d16ec551
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2016-02-22 13:09:46 -0300

    fixed required version of django-celery to 3.1.17

--- askbot/__init__.py
+++ askbot/__init__.py
@@ -21,7 +21,7 @@ REQUIREMENTS = {
     'compressor': 'django-compressor>=1.3,<=1.5',
     'django': 'django>=1.8,<1.9',
     'django_countries': 'django-countries==3.3',
-    'djcelery': 'django-celery>=3.0.11',
+    'djcelery': 'django-celery==3.1.17',
     'djkombu': 'django-kombu==0.9.4',
     'followit': 'django-followit==0.2.0',
     'html5lib': 'html5lib==0.90',
