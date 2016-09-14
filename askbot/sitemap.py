85b6ae59b45bde4c137ce4e0d1dfdfa9ade2a169
ZuluPro <montheanthony@hotmail.com>
2016-09-14 13:43:30 -0400

    PEP8ize sitemap.py

--- askbot/sitemap.py
+++ askbot/sitemap.py
@@ -1,14 +1,17 @@
 from django.contrib.sitemaps import Sitemap
 from askbot.models import Post
 
+
 class QuestionsSitemap(Sitemap):
     changefreq = 'daily'
     priority = 0.5
+
     def items(self):
         questions = Post.objects.get_questions()
         questions = questions.exclude(deleted=True)
         questions = questions.exclude(approved=False)
-        return questions.select_related('thread__title', 'thread__last_activity_at')
+        return questions.select_related('thread__title',
+                                        'thread__last_activity_at')
 
     def lastmod(self, obj):
         return obj.thread.last_activity_at
