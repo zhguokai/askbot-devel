846c73739f74e927176828d84fb16ba25050f392
ZuluPro <montheanthony@hotmail.com>
2016-09-14 13:42:42 -0400

    PEP8ize signals.py

--- askbot/signals.py
+++ askbot/signals.py
@@ -4,8 +4,8 @@ from collections import namedtuple
 
 import django.dispatch
 
-from django.db.models.signals import pre_save, post_save, pre_delete, post_delete, post_syncdb
-
+from django.db.models.signals import (pre_save, post_save,
+                                      pre_delete, post_delete, post_syncdb)
 
 try:
     from django.db.models.signals import m2m_changed
@@ -14,67 +14,49 @@ except ImportError:
 
 
 GenericSignal = namedtuple(
-    'GenericSignal',
-    field_names=['signal', 'callback', 'dispatch_uid']
-)
+    'GenericSignal', field_names=['signal', 'callback', 'dispatch_uid'])
 
 
 tags_updated = django.dispatch.Signal(
-                        providing_args=['tags', 'user', 'timestamp']
-                    )
+    providing_args=['tags', 'user', 'timestamp'])
 
 after_post_removed = django.dispatch.Signal(
     providing_args=['instance', 'deleted_by'])
+
 after_post_restored = django.dispatch.Signal(
     providing_args=['instance', 'restored_by'])
+
 flag_offensive = django.dispatch.Signal(providing_args=['instance', 'mark_by'])
 remove_flag_offensive = django.dispatch.Signal(providing_args=['instance', 'mark_by'])
 user_updated = django.dispatch.Signal(providing_args=['instance', 'updated_by'])
-user_registered = django.dispatch.Signal(providing_args=['user',])
-#todo: move this to authentication app
+user_registered = django.dispatch.Signal(providing_args=['user'])
+
+# TODO: move this to authentication app
 user_logged_in = django.dispatch.Signal(providing_args=['session'])
 
 new_answer_posted = django.dispatch.Signal(
-    providing_args=['answer', 'user', 'form_data']
-)
+    providing_args=['answer', 'user', 'form_data'])
 new_question_posted = django.dispatch.Signal(
-    providing_args=['question', 'user', 'form_data']
-)
+    providing_args=['question', 'user', 'form_data'])
 new_comment_posted = django.dispatch.Signal(
-    providing_args=['comment', 'user', 'form_data']
-)
+    providing_args=['comment', 'user', 'form_data'])
 answer_edited = django.dispatch.Signal(
-    providing_args=['answer', 'user', 'form_data']
-)
+    providing_args=['answer', 'user', 'form_data'])
 question_visited = django.dispatch.Signal(
-    providing_args=['request', 'question']
-)
+    providing_args=['request', 'question'])
 
 post_updated = django.dispatch.Signal(
-                                providing_args=[
-                                            'post',
-                                            'updated_by',
-                                            'newly_mentioned_users'
-                                        ]
-                            )
+    providing_args=['post', 'updated_by', 'newly_mentioned_users'])
+
 post_revision_published = django.dispatch.Signal(
-                                providing_args = [
-                                    'revision',
-                                    'was_approved'
-                                ]
-                            )
+    providing_args = ['revision', 'was_approved' ])
 
 spam_rejected = django.dispatch.Signal(
-                                providing_args = [
-                                    'text',
-                                    'spam',
-                                    'user',
-                                    'ip_addr'
-                                ]
-                            )
+    providing_args = ['text', 'spam', 'user', 'ip_addr' ])
 
 site_visited = django.dispatch.Signal(providing_args=['user', 'timestamp'])
 reputation_received = django.dispatch.Signal(providing_args=['user', 'reputation_before'])
+
 
 def pop_signal_receivers(signal):
     """disables a given signal by removing listener functions
@@ -84,11 +66,13 @@ def pop_signal_receivers(signal):
     signal.receivers = list()
     return receivers
 
+
 def set_signal_receivers(signal, receiver_list):
     """assigns a value of the receiver_list
     to the signal receivers
     """
     signal.receivers = receiver_list
+
 
 def pop_all_db_signal_receivers():
     """loops through all relevant signals
@@ -96,11 +80,11 @@ def pop_all_db_signal_receivers():
     dictionary where signals are keys
     and lists of receivers are values
     """
-    #this is the only askbot signal that is not defined here
-    #must use this to avoid a circular import
+    # this is the only askbot signal that is not defined here
+    # must use this to avoid a circular import
     from askbot.models.badges import award_badges_signal
     signals = (
-        #askbot signals
+        # askbot signals
         tags_updated,
         after_post_removed,
         after_post_restored,
@@ -111,7 +95,7 @@ def pop_all_db_signal_receivers():
         user_registered,
         post_updated,
         award_badges_signal,
-        #django signals
+        # django signals
         pre_save,
         post_save,
         pre_delete,
