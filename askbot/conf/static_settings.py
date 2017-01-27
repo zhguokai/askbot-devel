13222c07585a5ecfeac5ee49f36076d3a90a9250
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2017-01-28 03:24:42 +0700

    created defaults for the tinymce settings

--- askbot/conf/static_settings.py
+++ askbot/conf/static_settings.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.utils.translation import pgettext
 from appconf import AppConf
+import os
 
 class AskbotStaticSettings(AppConf):
     ALLOWED_UPLOAD_FILE_TYPES = ('.jpg', '.jpeg', '.gif',
@@ -34,3 +35,39 @@ class AskbotStaticSettings(AppConf):
 
     class Meta:
         prefix = 'askbot'
+
+
+class TinyMCESettings(AppConf):
+    COMPRESSOR = True
+    SPELLCHECKER = False
+    JS_ROOT = os.path.join(settings.STATIC_ROOT, 'default/media/tinymce/')
+    JS_URL = settings.STATIC_URL + 'default/media/tinymce/tiny_mce.js'
+    DEFAULT_CONFIG = {
+        'plugins': 'askbot_imageuploader,askbot_attachment',
+        'convert_urls': False,
+        'theme': 'advanced',
+        'content_css': settings.STATIC_URL + \
+                    'default/media/style/tinymce/content.css',
+        'force_br_newlines': True,
+        'force_p_newlines': False,
+        'forced_root_block': '',
+        'mode' : 'textareas',
+        'oninit': 'TinyMCE.onInitHook',
+        'plugins': 'askbot_imageuploader,askbot_attachment',
+        'theme_advanced_toolbar_location' : 'top',
+        'theme_advanced_toolbar_align': 'left',
+        'theme_advanced_buttons1': 'bold,italic,underline,|,bullist,numlist,|,undo,redo,|,link,unlink,askbot_imageuploader,askbot_attachment',
+        'theme_advanced_buttons2': '',
+        'theme_advanced_buttons3' : '',
+        'theme_advanced_path': False,
+        'theme_advanced_resizing': True,
+        'theme_advanced_resize_horizontal': False,
+        'theme_advanced_statusbar_location': 'bottom',
+        'editor_deselector': 'mceNoEditor',
+        'width': '100%',
+        'height': '250'
+    }
+
+
+    class Meta:
+        prefix = 'tinymce'
--- askbot/setup_templates/settings.py
+++ askbot/setup_templates/settings.py
@@ -257,34 +257,6 @@ ENABLE_HAYSTACK_SEARCH = False
 #            }
 #}
 
-TINYMCE_COMPRESSOR = True
-TINYMCE_SPELLCHECKER = False
-TINYMCE_JS_ROOT = os.path.join(STATIC_ROOT, 'default/media/tinymce/')
-TINYMCE_JS_URL = STATIC_URL + 'default/media/tinymce/tiny_mce.js'
-TINYMCE_DEFAULT_CONFIG = {
-    'plugins': 'askbot_imageuploader,askbot_attachment',
-    'convert_urls': False,
-    'theme': 'advanced',
-    'force_br_newlines': True,
-    'force_p_newlines': False,
-    'forced_root_block': '',
-    'mode' : 'textareas',
-    'oninit': "TinyMCE.onInitHook",
-    'plugins': 'askbot_imageuploader,askbot_attachment',
-    'theme_advanced_toolbar_location' : 'top',
-    'theme_advanced_toolbar_align': 'left',
-    'theme_advanced_buttons1': 'bold,italic,underline,|,bullist,numlist,|,undo,redo,|,link,unlink,askbot_imageuploader,askbot_attachment',
-    'theme_advanced_buttons2': '',
-    'theme_advanced_buttons3' : '',
-    'theme_advanced_path': False,
-    'theme_advanced_resizing': True,
-    'theme_advanced_resize_horizontal': False,
-    'theme_advanced_statusbar_location': 'bottom',
-    'editor_deselector': 'mceNoEditor',
-    'width': '100%',
-    'height': '250'
-}
-
 #delayed notifications, time in seconds, 15 mins by default
 NOTIFICATION_DELAY_TIME = 60 * 15
 
